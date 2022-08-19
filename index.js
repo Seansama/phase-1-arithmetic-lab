d89fa7363bd39f77e1351bab9538ffe4105b122a
ShaunM <seansama__@pop-os.localdomain>
2022-08-19 16:01:42 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+    let num1 = 31;
+    let num2 = 2;
+    let num3 = 16;
+    let num4 = 4;
 
-// Write your code here
+    let random = (Math.floor(Math.random()+ 1));
+    let multiply = num1*num2;
+    let mod = num3 / num4
+    let max = Math.max(2,4,6,20);
+ 
